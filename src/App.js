2a0aedc4bf8ee2508c0e3f03abd25ba30af7edb3
Ashish Yadav <ashish.k.yadav@globallogic.com>
2022-05-06 02:48:21 +0530

    Assignment Completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
-import React from 'react';
-import Dashboad from './Components/dashboad/Dashboad';
-import './App.css';
-import Footer from './Components/footer/Footer';
-import Header from './Components/header/Header';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+// import Footer from './Components/footer/Footer';
+// import Row from 'react-bootstrap/Row'
 
-class App extends React.Component{
-  render(){
-
-    return <div className = 'main-content' >
-     // call your components
-    </div>
-   
-  }
-}
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Dashboard
+              key="general"
+              pageSize={9}
+              api="addfc84c662c49ac8c356eee82f21d55"
+              category="general"
+            />
+          </Route>
+          <Route exact path="/general">
+            <Dashboard
+              key="general"
+              pageSize={9}
+              api="addfc84c662c49ac8c356eee82f21d55"
+              category="general"
+            />
+          </Route>
+          <Route exact path="/business">
+            <Dashboard
+              key="business"
+              pageSize={9}
+              api="addfc84c662c49ac8c356eee82f21d55"
+              category="business"
+            />
+          </Route>
+          <Route exact path="/entertainment">
+            <Dashboard
+              key="entertainment"
+              pageSize={9}
+              api="addfc84c662c49ac8c356eee82f21d55"
+              category="entertainment"
+            />
+          </Route>
+          <Route exact path="/health">
+            <Dashboard
+              key="health"
+              pageSize={9}
+              api="addfc84c662c49ac8c356eee82f21d55"
+              category="health"
+            />
+          </Route>
+          <Route exact path="/science">
+            <Dashboard
+              key="science"
+              pageSize={9}
+              api="addfc84c662c49ac8c356eee82f21d55"
+              category="science"
+            />
+          </Route>
+          <Route exact path="/sports">
+            <Dashboard
+              key="sports"
+              pageSize={9}
+              api="addfc84c662c49ac8c356eee82f21d55"
+              category="sports"
+            />
+          </Route>
+          <Route exact path="/technology">
+            <Dashboard
+              key="technology"
+              pageSize={9}
+              api="addfc84c662c49ac8c356eee82f21d55"
+              category="technology"
+            />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
+};
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,64 @@
+import React from "react";
+import axios from "axios";
+
+const Card = (props) => {
+  var myObjData = [];
+  let { title, description, imageUrl, newsUrl, author, date } = props;
+
+  const readLaterHandler = () => {
+    let myObj = {
+      newsUrl: newsUrl,
+      description: description,
+    };
+
+    console.log(myObj);
+    myObjData.push(myObj);
+    let json = JSON.stringify(myObjData);
+    console.log(json);
+
+    axios
+      .post("http://localhost:3001/readlater", myObj, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          // setReadLaterNews([...readLaterNews, response.data]);
+          console.log(response);
+        }
+      })
+      .catch((error) => console.log(error));
+
+    // firstly run this -> npm install axios
+    // writeJsonFile('C:\Users\Ashis\Desktop\React_boilerplate\react-assignment1-boilerplate\src\components\db.json', json);
+  };
+
+  return (
+    <>
+      <div className="my-3">
+        <div className="card">
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title.slice(0, 42)}...</h5>
+            <p className="card-text">{description.slice(0, 60)}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "unKnown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              // href={newsUrl}
+              // target="_blank"
+              onClick={readLaterHandler}
+              className="btn btn-sm btn-primary"
+            >
+              Read Later
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Card;
